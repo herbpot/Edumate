ca03c2291b5ba9125bbae37241589bebddb60254
sungwoo63 <138538926+sungwoo63@users.noreply.github.com>
2023-08-05 00:38:07 +0900

    수정

--- src/components/FileContent.tsx
+++ src/components/FileContent.tsx
@@ -1,19 +1,55 @@
-import { useRouter } from "next/router";
-import { ReactElement } from "react";
+import { useState, useEffect } from "react";
 
 const downloadModifiedFile = (filename: string = "test") => {
   fetch("/src/data/etcfile/" + filename)
     .then((res) => res.blob())
     .then((b) => {
       const url = URL.createObjectURL(b);
-      window.open(url);
+      const a = document.createElement("a");
+      a.href = url;
+      a.target = "_blank"; // Open the link in a new tab
+      a.click();
+      URL.revokeObjectURL(url);
     });
 };
 
-export default function FileContent({ name }: { name: string }) {
-    return (
-        <div key={Math.random()} id={name} className="etcfile" onClick={() => downloadModifiedFile(name)}> 
-            <h3 key={Math.random()}>{name}</h3>
-        </div>
-    );
+export default function FileContent({ id }: { id: string }) {
+  const [filelist, setFilelist] = useState<JSX.Element[] | null>(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/video/etcfile", {
+      method: "POST",
+      headers: {
+        id__: id,
+      },
+    })
+      .then((res) => res.json())
+      .then((j) => {
+        console.log(j);
+        const fileListElements = j.files.map((f: string) => (
+          <div key={f} onClick={() => downloadModifiedFile(f)}>
+            {f}
+          </div>
+        ));
+        setFilelist(fileListElements);
+      });
+  }, [id]);
+
+  return (
+    <div className="filecontent">
+      {filelist === null ? (
+        <div>Loading file list...</div>
+      ) : (
+        <>
+          {filelist}
+          <div
+            onClick={() => downloadModifiedFile()}
+            style={{ color: "white" }}
+          >
+            test
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
