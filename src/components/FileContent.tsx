cd2ad9283672465a82437a57dbac341772faad08
sungwoo63 <138538926+sungwoo63@users.noreply.github.com>
2023-08-04 23:28:05 +0900

    수정?

--- src/components/FileContent.tsx
+++ src/components/FileContent.tsx
@@ -1,7 +1,40 @@
-export default function FileContent() {
-    return(
-        <div className="filecontent">
-            
-        </div>
-    )
+import { useRouter } from "next/router";
+import { ReactElement } from "react";
+
+const downloadModifiedFile = (filename: string = "test") => {
+  fetch("/src/data/etcfile/" + filename)
+    .then((res) => res.blob())
+    .then((b) => {
+      const url = URL.createObjectURL(b);
+      window.open(url);
+    });
+};
+
+export default function FileContent({ id }: { id: string }) {
+  const filelist: ReactElement[] = [];
+
+  fetch("http://localhost:3000/api/video/etcfile", {
+    method: "POST",
+    headers: {
+      id__: id,
+    },
+  })
+    .then((res) => res.json())
+    .then((j) => {
+      console.log(j);
+      j.files.map((f: string) => {
+        filelist.push(
+          <div key={f} onClick={() => downloadModifiedFile(f)}>
+            {f}
+          </div>
+        );
+      });
+    });
+
+  return (
+    <div className="filecontent">
+      {filelist}
+      <div onClick={() => downloadModifiedFile()}>{"test"}</div>
+    </div>
+  );
 }
