a01b45088bf6d5c1f380798ea1dc363ac0e209e5
sungwoo63 <138538926+sungwoo63@users.noreply.github.com>
2023-08-05 21:38:58 +0900

    수정 필요

--- src/components/FileContent.tsx
+++ src/components/FileContent.tsx
@@ -1,23 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-const downloadFile = (filename: string) => {
-  const fileURL = `/src/data/etcfile/${encodeURIComponent(filename)}`;
-  fetch(fileURL)
-    .then((response) => response.blob())
-    .then((blob) => {
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.href = url;
-      a.download = filename;
-      a.click();
-      URL.revokeObjectURL(url);
-    })
-    .catch((error) => {
-      console.error("Error downloading file:", error);
-      // Handle the error here, e.g., show an error message to the user
-    });
-};
-
 export default function FileContent() {
   const [filelist, setFilelist] = useState<string[]>([]);
 
@@ -25,23 +7,40 @@ export default function FileContent() {
     fetchFiles();
   }, []);
 
-  const fetchFiles = async () => {
-    const response = await fetch("/src/data/etcfile");
-    if (response.ok) {
-      const files = await response.json();
-      setFilelist(files);
-    } else {
-      console.error(
-        "Error fetching file list:",
-        response.status,
-        response.statusText
-      );
-    }
+  const fetchFiles = () => {
+    fetch("/src/data/etcfile") // 올바른 경로로 수정
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `Error fetching file list: ${response.status} ${response.statusText}`
+          );
+        }
+        return response.json();
+      })
+      .then((files) => {
+        setFilelist(files);
+      })
+      .catch((error) => {
+        console.error("Error fetching file list:", error);
+      });
   };
 
-  const handleFileClick = (filename: string) => {
-    // 클릭한 파일을 다운로드
-    downloadFile(filename);
+  const downloadFile = (filename: string) => {
+    const fileURL = `/src/data/etcfile/${encodeURIComponent(filename)}`;
+    fetch(fileURL)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = filename;
+        a.click();
+        URL.revokeObjectURL(url);
+      })
+      .catch((error) => {
+        console.error("Error downloading file:", error);
+        // Handle the error here, e.g., show an error message to the user
+      });
   };
 
   if (filelist.length === 0) {
@@ -50,10 +49,10 @@ export default function FileContent() {
 
   return (
     <div className="filecontent">
-      {filelist.map((filename) => (
+      {filelist.map((filename: string, index: number) => (
         <div
-          key={filename}
-          onClick={() => handleFileClick(filename)}
+          key={index}
+          onClick={() => downloadFile(filename)}
           style={{ color: "white", cursor: "pointer" }}
         >
           {filename}
